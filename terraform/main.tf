b3cdad5dad5cd1f1920eb92f238c83e3e414b5a0
Sujith Quintelier <sujith.quintelier@gmail.com>
2022-02-11 18:12:55 +0100

    add st

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group" "rg" {
   name     = join("-", ["rg", local.vs, local.env, "01"])
   location = "westeurope"
 }
+
+resource "azurerm_storage_account" "st" {
+  name                = join("-", ["st", local.vs, local.env, "01"])
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
